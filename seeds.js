61c7ccfc72ae97c835d6e89b8b705566941ed57e
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-10-03 14:22:11 +0530

    Update seeds.js
    DB seeding bug has been fixed.

--- seeds.js
+++ seeds.js
@@ -29,55 +29,16 @@ var data = [
     }
 ]
 
-
-    // File.deleteMany({},(err)=>{
-    //     if(err){
-    //         console.err(err);
-    //     } else {
-    //         console.log('db has beem seeded!')
-    //     }
-
-    //     data.forEach((seed)=>{
-    //         file.create(seed, (err, file)=>{
-    //             if(err){
-    //                 console.err(err);
-    //             } else {
-    //                 console.log('file has been added.')
-    //             }
-    //         });
-    //     });
-    // });
-
-    // try{
-    //     File.deleteMany({name: "tf bro?", description: "tf bro?"}, (err) =>{
-    //         if(err){
-    //             console.err(err);
-    //         } else {
-    //             console.log('db has been seeded!');
-    //         }
-
-    //         data.forEach((seed) =>{
-    //             if(err){
-    //                 console.err(err);
-    //             } else {
-    //                 console.log('file has been addded!');
-    //             }
-    //         });
-    //     });
-
-
     async function seed(){
         try{
             await File.deleteMany();
             console.log('DB has been seeded!');
-            await data.forEach((seed)=>{
-                File.create(seed, (err, File)=>{
-                    if(err){
-                        console.error(err);
-                    } else{
-                        console.log("File has been added!");
-                    }
-                });              
+            await File.create(data).then((err)=>{
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log('File has been created!')
+                }
             });
         } catch(err){
             console.log(err);
