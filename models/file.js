0b3deb3c872e9a0de72a3f37da7df4ab9814a4ca
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-10-08 12:05:19 +0530

    Files Edited
    - Login form edited
    - File Schema edited

--- models/file.js
+++ models/file.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 
 const fileSchema = new mongoose.Schema({
     name: String,
-    description: String
-    // file: String,
-    // owner: {
-    //     id:{
-    //         type: mongoose.Schema.Types.ObjectId,
-    //         ref: 'user'
-    //     },
-    //     name: String,
-    //     username: String,
-    //     pfp: String
-    // }
+    description: String,
+    file: String,
+    owner: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        name: String,
+        username: String,
+        pfp: String
+    }
 });
 
 module.exports = mongoose.model('File', fileSchema);
