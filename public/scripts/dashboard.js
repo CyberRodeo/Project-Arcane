5817467bb358547a623780830589f876e369625b
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-11-13 15:57:59 +0530

    scripts cleaned

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -2,7 +2,4 @@ function redirect(){
     window.location.href = "/user/profile";
 }
 
-var dotenv = require('dotenv');
-
-dotenv.config();
 
--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -9,10 +9,7 @@ function backpage(){
 function changeText(){
     if(inp.files.length > 0){
         var text = document.getElementById('text');
-        // console.log('Logging text' + text);
         var name = inp.files[0].name;
         text.innerText = "You have chosen: \n" +name;
-        // console.log('logging name' + name);
-        // console.log('YO HO HO HO, YO  HO HO HO')
     }    
 };
