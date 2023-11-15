f4af687b06a153f0a83a6875509161a40a01f6c3
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-11-15 23:25:47 +0530

    bug fixes

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('../logs/logs');
 
 require('dotenv').config();
 
@@ -7,7 +8,7 @@ var middlewareobj = {};
 middlewareobj.authMiddleware = function(req, res, next){
     if(process.env.isLoggedin == 'false'){
         res.redirect('/');
-        console.log('the value of the variable is false bruh');
+        logger('User is not logged in, no access granted!');
     } else {
         next();
     }
@@ -26,6 +27,7 @@ middlewareobj.userLogged = function(req, res, next){
     if(process.env.isloggedin == 'true'){
         next();
     } else {
+        logger('No user session detected, no access granted!')
         res.redirect('/login');
     }
 };
@@ -33,6 +35,7 @@ middlewareobj.userLogged = function(req, res, next){
 
 middlewareobj.upldMidwre = function(req, res, next){
     if(process.env.isloggedin == 'false'){
+        logger('No user session detected, no access granted!')
         res.redirect('/login');
     } else {
         next();
@@ -42,7 +45,7 @@ middlewareobj.upldMidwre = function(req, res, next){
 middlewareobj.logoutmidwre = function(req, res, next){
     if(process.env.isLoggedin == 'false'){
         res.redirect('login');
-        console.error('No User session detected, redirected to login page.')
+        logger('No User session detected, redirected to login page.')
     } else {
         next();
     }
