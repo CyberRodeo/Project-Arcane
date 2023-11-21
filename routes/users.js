38ce396113cc04735115440081a0b89b6fab65f4
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-11-21 18:33:30 +0530

    userSession now works on editprofile route

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const File = require('../models/file');
 const middlewareobj = require("../middleware/index");
 const logger = require('../logs/logs');
+const usrSession = require('../userSession/session');
 
 require('dotenv').config();
 
 router.get('/profile', middlewareobj.userLogged, (req, res) => {
-    res.render('user/editProfile');
+    UserSession = usrSession.fetchUserSession();
+    res.render('user/editProfile', {userSession: UserSession});
 }); 
 
 router.get('/', (req, res) =>{
