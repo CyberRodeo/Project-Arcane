da9cb17e96cf7f7bb2df62bc410cd07f36c33e3e
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-12-24 22:53:39 +0530

    code cleaning

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,5 +1,24 @@
-function redirect(){
-    window.location.href = "/user/profile";
+const files = document.getElementsByClassName("parentFiles");
+const fileActionsD = document.getElementById("downloadAction");
+
+console.log(files);
+
+console.log(files.length);
+
+function event(){
+    console.log('hovered!!!');
+};
+for(i=0;i <= files.length; i++){
+   files[i].addEventListener('mouseenter', event());
 }
 
+// function hover(){
+//     files.addEventListener("mouseenter", console.log('Files have been hovered on!'));
+// };
 
+// hover();
+
+
+function redirect(){
+    window.location.href = "/user/profile";
+};
