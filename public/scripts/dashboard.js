34f48f4d17c18b69e75f363f636e81f44bba157b
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-12-25 05:51:08 +0530

    Update dashboard.js

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,15 +1,27 @@
-const files = document.getElementsByClassName("parentFiles");
+const files = document.getElementsByClassName("files");
 const fileActionsD = document.getElementById("downloadAction");
 
 console.log(files);
 
 console.log(files.length);
 
-function event(){
+function hoverEvent(){
     console.log('hovered!!!');
 };
+
+
 for(i=0;i <= files.length; i++){
-   files[i].addEventListener('mouseenter', event());
+   files[i].addEventListener('mouseenter', ()=>{
+        fileActionsD.classList.add('hide');
+        fileActionsD.classList.remove('show');
+        console.log('hovered!');
+   });
+
+   files[i].addEventListener('mouseleave', ()=>{
+        fileActionsD.classList.add('show');
+        fileActionsD.classList.remove('hide');
+        console.log('hovered out!');
+   });
 }
 
 // function hover(){
