5164a53211a065a6fb76a6d487589d129964a77f
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2024-01-05 16:03:01 +0530

    2 new routes created

--- routes/files.js
+++ routes/files.js
@@ -20,4 +20,12 @@ router.get('/files', (req, res)=>{
     res.render('errorhandling/error404', {Reason: "Whether there is no such file with id or you are on the wrong link"});
 });
 
+router.get(':id/edit',(req, res) => {
+    res.send("this is the edit page for file !");
+});
+
+router.get(':id/delete', (req, res)=>{
+    res.send('file deleted!');
+});
+
 module.exports = router;
