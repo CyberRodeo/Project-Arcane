f2eed865db9e93da2098cb66916f7321dc7c8a87
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-01-27 07:17:47 +0530

    Update main-server.js

--- main-server.js
+++ main-server.js
@@ -1,10 +1,13 @@
+const { GuildDefaultMessageNotifications } = require('discord.js');
+const { GridFsStorage } = require('multer-gridfs-storage');
+
 const express = require('express'),
       app = express(),
       mongoose = require('mongoose'),
       passport = require('passport'),
       passport_local = require('passport-local'),
       body_parser = require('body-parser'),
-      PORT = 8000,
+      PORT = 3000,
       userRoutes = require('./routes/users'),
       dashroutes = require('./routes/dashboard'),
       filesroutes = require('./routes/files'),
@@ -15,16 +18,42 @@ const express = require('express'),
       middleware = require('./middleware/index'),
       logger = require('./logs/logs'),
       multer = require('multer'),
-      gridfs = require('multer-gridfs-storage');
+      gridfs = require('multer-gridfs-storage'),
+      methodOverride = require('method-override'),
+      grid = require('gridfs-stream');
       
 require('dotenv').config();
 
-mongoose.connect(process.env.localDatabase, {
+const dbconn = mongoose.connect(process.env.localDatabase, {
 }).then(() =>{
     logger('Arcane DB has been connected successfully!');
 }).catch(err => {
     console.err("ERROR",err.message);
 });
+
+// let gfs;
+
+// dbconn.once('open', () => {
+//     gfs = Grid(dbconn.db, mongoose,mongo);
+//     gfs.collections('uploads');
+// });
+
+const storage = new GridFsStorage({
+    url: process.env.localDatabase,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+                const fileName = file.originalname;
+                const fileinfo = {
+                    filename: fileName,
+                    bucketname: 'uploads'
+                };
+                resolve(fileinfo);
+        });
+    }
+});
+
+const upload = multer({storage});
+
 
 
 logger('I Love Computers - CyberRodeo');
@@ -38,6 +67,10 @@ app.use(indexRoutes);
 app.use("/dashboard", dashroutes);
 app.use("/user", userRoutes);
 app.use("/file", filesroutes);
+
+app.post('/upload', upload.single('file', (req, res) => {
+    res.redirect('/dashboard');
+}));
 
 
 app.listen(PORT || process.env.PORT, process.env.IP,  function(){
