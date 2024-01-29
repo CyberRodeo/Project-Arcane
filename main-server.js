fc0414973ad5c1b1d35e34caa8e794105517e6bc
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-01-29 06:15:17 +0530

    Update main-server.js

--- main-server.js
+++ main-server.js
@@ -17,44 +17,17 @@ const express = require('express'),
       seedDB = require('./seeds'),
       middleware = require('./middleware/index'),
       logger = require('./logs/logs'),
-      multer = require('multer'),
-      gridfs = require('multer-gridfs-storage'),
-      methodOverride = require('method-override'),
-      grid = require('gridfs-stream');
+      multer = require('multer');
+
       
 require('dotenv').config();
 
-const dbconn = mongoose.connect(process.env.localDatabase, {
+mongoose.connect(process.env.localDatabase, {
 }).then(() =>{
     logger('Arcane DB has been connected successfully!');
 }).catch(err => {
     console.err("ERROR",err.message);
 });
-
-// let gfs;
-
-// dbconn.once('open', () => {
-//     gfs = Grid(dbconn.db, mongoose,mongo);
-//     gfs.collections('uploads');
-// });
-
-const storage = new GridFsStorage({
-    url: process.env.localDatabase,
-    file: (req, file) => {
-        return new Promise((resolve, reject) => {
-                const fileName = file.originalname;
-                const fileinfo = {
-                    filename: fileName,
-                    bucketname: 'uploads'
-                };
-                resolve(fileinfo);
-        });
-    }
-});
-
-const upload = multer({storage});
-
-
 
 logger('I Love Computers - CyberRodeo');
 // seedDB.seed();
@@ -67,11 +40,6 @@ app.use(indexRoutes);
 app.use("/dashboard", dashroutes);
 app.use("/user", userRoutes);
 app.use("/file", filesroutes);
-
-app.post('/upload', upload.single('file', (req, res) => {
-    res.redirect('/dashboard');
-}));
-
 
 app.listen(PORT || process.env.PORT, process.env.IP,  function(){
     logger("Arcane server is up on PORT:", PORT)
