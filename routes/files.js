8a9b6b3f7eb79073bae93f82c608ac0cacfd673a
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-01-30 17:18:53 +0530

    upload bug fixed

--- routes/files.js
+++ routes/files.js
@@ -16,15 +16,15 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, './uploads')
     },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.orinignalname + '-' + uniqueSuffix)
+    filename: function(req, file, cb){
+        cb(null,file.originalname)
     }
   })
   
   const upload = multer({ storage: storage })
 router.post('/upload', upload.single('file'), (req, res) => {
     logger('File has been uploaded');
+    console.log(req.file);
     res.redirect('/');
 });
 
