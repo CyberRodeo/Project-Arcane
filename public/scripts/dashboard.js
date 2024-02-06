613860449da00db3e803471440745daa86af9ebb
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-02-06 05:51:48 +0530

    code cleaning

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,33 +1,3 @@
-// const parentFiles = document.getElementsByClassName('parentFiles');
-// const downloadBtn = document.getElementsByClassName('filesActions');
-
-// const files = Array.from(parentFiles);
-// const actions = Array.from(downloadBtn);
-
-// console.log(actions);
-// const fl = files.length;
-// console.log(fl);
-
-// function shows(s){
-//     for(i = 0; i < actions.length; i++){
-//         s[i].classlist.add('shows');
-//         s[i].classlist.remove('hides');
-//     };  
-// };
-
-// function hides(h){
-//     for(i = 0; i < actions.length; i++){
-//         h[i].classlist.add('hides');
-//         h[i].classlist.remove('shows');
-//     };
-// };
-
-
-// for(const Files of files){
-//     Files.addEventListener('mouseenter', shows(actions), false);
-//     Files.addEventListener('mouseleave', hides(actions), false);
-// };
-
 function redirect(){
     window.location.href = "/user/profile";
 };
